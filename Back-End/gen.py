66eb87172e134a5397178be19bcf1f794e2f21d6
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-02-05 20:31:10 +0530

    langchain

--- Back-End/gen.py
+++ Back-End/gen.py
@@ -1,5 +1,6 @@
 import os
 import google.generativeai as genai
+from langchain.prompts import PromptTemplate
 
 
 class Solution(object):
@@ -38,13 +39,15 @@ class Solution(object):
         self._model = genai.GenerativeModel('gemini-pro')
         
         self._skinType=skinType
-        self._prompt= f"Identify this skin disease as a blackheads and assume that this skin disease on a {self._skinType} skin. please genarate a complete description about the disease, why this disease occurs and genarate that ways can be use to prevent and avoid this skin disease at descriptive manner."
-                        
+        self._prompt= "Identify this skin disease as a blackheads and assume that this skin disease on a {skinType} skin. please genarate a complete description about the disease, why this disease occurs and genarate that ways can be use to prevent and avoid this skin disease at descriptive manner."
+        self._template = PromptTemplate(input_variables=['skinType'], 
+                                        template=self._prompt)
+          
                         
         
     def geminiResponce(self) -> str:
         try:
-            self._response=self._model.generate_content(self._prompt, stream=False)
+            self._response=self._model.generate_content(self._template.format(skinType=self._skinType), stream=False)
             for chunk in self._response:
                self._response_ = ''.join(chunk.text) 
             
