c52fbb2b2539afbff7d7593aa64eed69fd80535f
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-02-08 00:42:36 +0530

    Login eka hari yako

--- Back-End/app.py
+++ Back-End/app.py
@@ -1,29 +1,66 @@
 from flask import Flask, request, jsonify, session
-from  flask_mysqldb import MySQL
-import MySQLdb.cursors
+from flask_bcrypt import Bcrypt
+from pymongo import MongoClient
 import gen
 
 app=Flask(__name__)
+app.secret_key = "123456789"
+bcrypt = Bcrypt(app)
 
-#MySQl configurations
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'Glova'
-
-mysql = MySQL(app)
+#DB configurations
+client = MongoClient('mongodb://localhost:27017')
+db = client['Glova']
 
 
 @app.route("/sign-in", methods=['GET', 'POST'])
 def signIn():
-    return "signIn"
+    emailAddress = str(request.args['emailAddress'])
+    password = str(request.args['password'])
+    collection = db['Users']
+    
+    user = collection.find_one({"emailAddress" : emailAddress})
+    if (user == None):
+        responce = False
+    else:
+        isValid = bcrypt.check_password_hash(user['password'], password)
+        if(isValid):
+            responce = user['username']
+            
+            session['loggedIn'] = True
+            session['username'] = user['username']
+            
+        else:
+            responce = False
+        
+    return jsonify({'responce' : responce})
 
 
 @app.route("/sign-up", methods=['GET', 'POST'])
 def signUp():
-    return "signup"
+    username = str(request.args['username'])
+    emailAddress = str(request.args['emailAddress'])
+    phoneNumber = str(request.args['phoneNumber'])
+    password = str(request.args['password'])
+        
+    collection = db['Users']
+    
+    ifExsist = collection.find_one({"emailAddress" : emailAddress})
+    
+    if (ifExsist == None):
+        password = bcrypt.generate_password_hash(password).decode('utf-8')
+        collection.insert_one({"username" : username, "emailAddress" : emailAddress, "phoneNumber" : phoneNumber, "password" : password})
+        responce = True
+    else:
+        responce = False
+    
+    return jsonify({'responce': responce})
 
-
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    session.pop('loggedIn', None)
+    session.pop('username', None)
+    
+    
 @app.route("/data", methods=['GET', 'POST'])
 def data():
     return "data"
